71816f44fbd1e7d1ce128704e90688218fdc364e
Duncan Garing <duncan.garing@bellevuecollege.edu>
2021-06-15 18:44:55 -0700

    fixed error, added 2 more test cases

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -16,13 +16,15 @@ public class CILabTest {
         myString = null;
     }
 
+    //test no capital letters
     @Test
     public void detectCapitalUseTest1() {
        myString.setString("yup yup");
        boolean capital = myString.detectCapitalUse();
-       assertEquals(false, capital);
+       assertEquals(true, capital);
     }
 
+    //test first letter capital
     @Test
     public void detectCapitalUseTest2() {
         myString.setString("Yup yup");
@@ -30,5 +32,19 @@ public class CILabTest {
         assertEquals(true, capital);
     }
 
+    //testing capitals in middle of string
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("yup YAH yup");
+        boolean capital = myString.detectCapitalUse();
+        assertEquals(false, capital);
+    }
 
+    //testing all capitals in string
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("YUPYUP");
+        boolean capital = myString.detectCapitalUse();
+        assertEquals(true, capital);
+    }
 }
